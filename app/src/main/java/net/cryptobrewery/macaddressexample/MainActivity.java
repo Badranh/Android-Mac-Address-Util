157310a1b5e4f9d41413a3e47aee2e9ff90a675b
TOSHIBA <hasanbadran88@gmail.com>
2018-06-25 00:14:02 +0300

    Created MacAddress Library . Created Test App

--- app/src/main/java/net/cryptobrewery/macaddressexample/MainActivity.java
+++ app/src/main/java/net/cryptobrewery/macaddressexample/MainActivity.java
@@ -2,12 +2,15 @@ package net.cryptobrewery.macaddressexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import net.cryptobrewery.macaddress.MacAddress;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Log.d("testing", MacAddress.getMacAddr());
     }
 }
